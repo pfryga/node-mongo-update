6b0fe2bd5c46ce71cddac800a2d7f6020ba16c13
Piotr Fryga <piotr.fryga@allegro.pl>
2015-04-14 22:07:46 +0200

    update of selected document

--- app.js
+++ app.js
@@ -8,4 +8,12 @@ MongoClient.connect('mongodb://localhost:27017/blog', function(err, db) {
     if(err) throw err;
 
     console.log('Connected to database!');
+
+    var posts = db.collection('posts');
+
+    posts.find({ 'permalink': 'bjdgkubdbesvrgkvvelt' }).toArray(function (err, items) {
+        posts.update({ _id: items[0]._id }, { $set: { author: 'test123'} }, function (err, result) {
+            console.log(result);
+        });
+    });
 });
